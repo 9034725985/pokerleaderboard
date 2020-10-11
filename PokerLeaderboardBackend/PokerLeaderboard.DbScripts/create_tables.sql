eee986cab7a09e0fb757ffb1bb9a34a2a92d38cc
kushal <kushaldeveloper@gmail.com>
2020-10-11 08:15:53 -0600

    update table structure

--- PokerLeaderboardBackend/PokerLeaderboard.DbScripts/create_tables.sql
+++ PokerLeaderboardBackend/PokerLeaderboard.DbScripts/create_tables.sql
@@ -5,17 +5,19 @@ drop table  lookup_country;
 
 create table lookup_country
 (
-  id varchar(255) default gen_random_uuid() primary key,
+  id integer primary key generated always as identity,
+  external_id varchar(255) default gen_random_uuid() unique not null,
   full_name varchar(255) not null,
   abbreviation varchar(10) not null
 );
 
 create table person
 (
-  id varchar(255) default gen_random_uuid() primary key,
+  id integer primary key generated always as identity,
+  external_id varchar(255) default gen_random_uuid() unique not null,
   full_name varchar(255) not null,
   winnings decimal(19,4) not null default 0,
-  country varchar(255) not null references lookup_country (id)
+  country varchar(255) not null references lookup_country (external_id)
 );
 
 insert into lookup_country
@@ -26,8 +28,8 @@ values
 ('United States of America', 'USA'),
 ('Great Britain', 'GBR');
 
-insert into person(full_name,country)
-select person_name,id
+insert into person(full_name, country)
+select person_name, external_id
 from 
 (
     values 
@@ -36,6 +38,6 @@ from
     ('Erik Seidel'),
     ('Phil Hellmuth')
 ) as person (person_name)
-cross join (select id from lookup_country where abbreviation = 'USA') c;
+cross join (select external_id from lookup_country where abbreviation = 'USA') c;
 
 select * from person;
