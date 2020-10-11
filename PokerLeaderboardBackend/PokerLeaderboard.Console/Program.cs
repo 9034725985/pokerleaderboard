6e47785015714624f531cd07f236098e65fcf8c5
kushal <kushaldeveloper@gmail.com>
2020-10-11 09:03:00 -0600

    move database connection code to Infrastructure

--- PokerLeaderboardBackend/PokerLeaderboard.Console/Program.cs
+++ PokerLeaderboardBackend/PokerLeaderboard.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using PokerLeaderboard.Infrastructure;
 
 namespace PokerLeaderboard.Console
 {
@@ -6,7 +6,10 @@ namespace PokerLeaderboard.Console
     {
         static void Main(string[] args)
         {
-             
+            string connectionString = "Host=balarama.db.elephantsql.com;Port=5432;Username=xukiiogf;Password=REkQDxhkxXdSd9Yj7gbj75ovitfPxr7k;Database=xukiiogf;Integrated Security=true;Pooling=true;Timeout=30";
+
+            Class1 class1 = new Class1();
+            class1.Print(connectionString);
         }
     }
 }
